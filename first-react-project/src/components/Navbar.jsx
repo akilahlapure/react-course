0f41be20e80f6b61dd822211aba7248710b7b362
akickandopen <akilah.lapure@gmail.com>
2022-09-22 16:56:26 +0400

    build static website with react

---
+++ first-react-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <img className="logo-img" src="src/assets/react.svg"/>
+                <h1 className="logo-title">ReactFacts</h1>
+            </div>
+            <h2 className="project-name">React Course - Project 1</h2>
+        </nav>
+    )
+}
