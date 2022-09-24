ce9aa2a9dce6d6615767452d785ad732a5861c1c
akickandopen <akilah.lapure@gmail.com>
2022-09-24 15:18:02 +0400

    complete info section structure

---
+++ digital-business-card-project/src/components/Info.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <section id="info">
+            <img src="src/assets/akilah-lapure.jpg"/>
+            <div className="info-details">
+                <h1 className="name">Akilah Lapure</h1>
+                <p class="title">Web Developer</p>
+                <p class="site">github.com/akilahlapure</p>
+            </div>
+            <div className="buttons">
+                <button className="btn btn-email">
+                    <i className="fa-solid fa-envelope btn-icon"></i>
+                    <p>Email</p>
+                </button>
+                <button className="btn btn-linkedin">
+                    <i className="fa-brands fa-linkedin btn-icon"></i>
+                    <p>LinkedIn</p>
+                </button>
+            </div>
+        </section>
+    )
+}
