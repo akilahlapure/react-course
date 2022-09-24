ca679c46c166f047bd6f880c37af0aedc1bc78a6
akickandopen <akilah.lapure@gmail.com>
2022-09-24 15:35:16 +0400

    add html structure and content

--- digital-business-card-project/src/components/About.jsx
+++ digital-business-card-project/src/components/About.jsx
@@ -1,4 +1,13 @@
 import React from "react"
 
 export default function About() {
+    return (
+        <section id="about">
+            <h3 className="content-heading">About</h3>
+            <p className="content">
+                A fresh graduate of creative computing with experience on web, android, and game development. 
+                Most proficient in web development and design using my knowledge on coding languages, programs and designing tools. 
+            </p>
+        </section>
+    )
 }
--- digital-business-card-project/src/components/Interests.jsx
+++ digital-business-card-project/src/components/Interests.jsx
@@ -1,4 +1,13 @@
 import React from "react"
 
 export default function Interests() {
+    return (
+        <section id="interests">
+            <h3 className="content-heading">Interests</h3>
+            <p className="content">
+                Besides designing and developing, my other interests are pretty normal. I love singing and drawing
+                although I'm not that good at it hehe. My other passion would be BTS. just them. i love them.
+            </p>
+        </section>
+    )
 }
