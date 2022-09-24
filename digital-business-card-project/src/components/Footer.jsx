b741f0d20c6c263a0099f6ff347d35296c9a672b
akickandopen <akilah.lapure@gmail.com>
2022-09-24 16:19:02 +0400

    complete footer component

--- digital-business-card-project/src/components/Footer.jsx
+++ digital-business-card-project/src/components/Footer.jsx
@@ -1,4 +1,12 @@
 import React from "react"
 
 export default function Footer() {
+    return (
+        <footer>
+            <i className="fa-brands fa-square-twitter social-icon"></i>
+            <i className="fa-brands fa-square-facebook social-icon"></i>
+            <i className="fa-brands fa-square-instagram social-icon"></i>
+            <i className="fa-brands fa-square-github social-icon"></i>
+        </footer>
+    )
 }
