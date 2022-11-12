e54308f382df67f2068cec553d568af45194fcd4
akickandopen <akilah.lapure@gmail.com>
2022-11-12 16:45:01 +0400

    get new image on button click using React State

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,18 +2,20 @@ import React from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
     
     function getNewMeme() {
         const memeArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memeArray.length) 
         const url = memeArray[randomNumber].url
 
-        console.log(url)
+        setMemeImage(url)
     }
 
     return (
-        <section>
-            <div className="form">
+        <main>
+            <section className="form">
                 <div className="input-text">
                     <input type="text" placeholder="Shut Up"/>
                     <input type="text" placeholder="and take my money"/>
@@ -21,7 +23,10 @@ export default function Meme() {
                 <button className="input-submit" type="submit" onClick={getNewMeme}>
                     <p>Get a New Meme Image 🖼</p>
                 </button>
-            </div>
-        </section>
+            </section>
+            <section className="meme-image">
+                <img src={memeImage} alt="Meme Image" />
+            </section>
+        </main>
     )
 }
